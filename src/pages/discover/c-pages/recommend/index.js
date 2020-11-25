44602536c2172ee18887dc80de4737ee85f8aadc
wodezxzl <2534618940@qq.com>
2020-11-25 09:11:55 +0800

    使用Hook重构redux

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,24 +1,22 @@
 import React, { memo, useEffect } from 'react'
-import { connect } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
 import { getTopBannerAction } from './store/actionCreators'
 
-function Recommend(props) {
-  const { getBanners, topBanners } = props
+function Recommend() {
+  const { topBanners } = useSelector(
+    (state) => ({
+      topBanners: state.recommend.topBanners
+    }),
+    shallowEqual
+  )
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    getBanners()
-  }, [getBanners])
+    dispatch(getTopBannerAction())
+  }, [dispatch])
 
   return <div>Recommend: {topBanners.length}</div>
 }
 
-const mapStateToProps = (state) => ({
-  topBanners: state.recommend.topBanners
-})
-const mapDispatchToProps = (dispatch) => ({
-  getBanners: () => {
-    dispatch(getTopBannerAction())
-  }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(memo(Recommend))
+export default memo(Recommend)
